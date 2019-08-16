baa7aca2ad992b76ab51dff1d9c80e04d951363f
Lev Babchenko <lvbabchenko@yandex-team.ru>
2019-08-16 22:01:05 +0300

    20190816_220105

--- sync.sh
+++ sync.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 base=$(dirname $(readlink -f $0))
 cd $base
+branch=$(git branch | awk '/^\* /{print gensub(/^../, "", $0);}')
 git fetch --all
-git pull
+git pull origin $branch
 
 cd $base/../metagather
 python3.6 run-merge.py
 
 cd $base
 git commit -a -m "$(date +'%Y%m%d_%H%M%S')"
-git push origin
+git push origin $branch
